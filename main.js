00ba8958de786961e7edaeb83f38d48fcf7e2e2f
Scott <lowt3k@hotmail.com>
2015-03-14 22:36:37 -0700

    change name from Make Em Happy to Slap'Em Happy commented out the frame asset added the faces_sheet sprite sheet and removed the individual face calls

--- main.js
+++ main.js
@@ -14,15 +14,10 @@ var mainState = { // create the 'main' state, contains the game
 
   preload: function() {
     // --== User Interface assets ==--
-    game.load.image('frame', 'assets/game_play/frame.png');
-    game.load.image('cursor', 'assets/game_play/feather.png');
+    //game.load.image('frame', 'assets/game_play/frame.png');
     
     // --== Game Play assets ==--
-    game.load.image('happy', 'assets/game_play/happy.png');
-    game.load.image('neutral', 'assets/game_play/neutral.png');
-    game.load.image('unhappy', 'assets/game_play/unhappy.png');
-    game.load.image('mad', 'assets/game_play/mad.png');
-    game.load.image('angry', 'assets/game_play/angry.png');
+    game.load.spritesheet('faces', 'assets/game_play/faces_sheet.png', 256, 256, 5); // load a sprite sheet with all five faces
   }, // preload
   
   create: function() {
@@ -30,7 +25,7 @@ var mainState = { // create the 'main' state, contains the game
     
     this.mouseInputs = 0;
     
-    this.face = game.add.sprite(480, 320, 'happy'); // assign happy face to a variable
+    this.face = game.add.sprite(480, 320, 'faces'); // assign happy face to a variable
     
     this.face.anchor.set(0.5); // move pivot point (anchor) to the centre of the sprite
     this.face.scale.set(0.5); // scale sprite down
@@ -39,10 +34,10 @@ var mainState = { // create the 'main' state, contains the game
     this.face.input.pixelPerfectClick = true; //ignore the transparent area around the image
     this.face.events.onInputDown.add(this.faceTouched, this); // listen for input on the sprite
         
-    game.add.sprite(0, 0, 'frame'); // this is the frame and must be rendered on top of everything
+    //game.add.sprite(0, 0, 'frame'); // this is the frame and must be rendered on top of everything
     
     // --== display debug info ==--  
-    this.debugText = game.add.text(10, 24, 'debug: ' + this.mouseInputs); // text    
+    this.debugText = game.add.text(10, 10, 'debug: ' + this.mouseInputs); // text    
     
   }, // create
   
